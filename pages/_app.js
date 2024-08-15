415f8d316583c8c3ca6bc62bd5e424fdb2fb6e76
xxcyl <wittyxx@gmail.com>
2024-08-15 14:18:11 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import { Analytics } from "@vercel/analytics/react"
 import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
