c71aa5d5e5bf2c729393a36b2ee3717a75ae088f
xxcyl <wittyxx@gmail.com>
2024-09-26 21:05:49 +0800

    Update RssViewer.js

--- components/RssViewer.js
+++ components/RssViewer.js
@@ -41,7 +41,7 @@ const RssViewer = () => {
   
   const setCurrentPageAndScrollToTop = (pageNumber) => {
     setCurrentPage(pageNumber);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo(0, 0);
   };
   
   const fetchStats = async () => {
